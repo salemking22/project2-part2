8f491ac840a1bc2dea12044fee89fb7b64273434
salemking22 <brynjerel@gmail.com>
2025-10-01 10:51:52 +0200

    Fix: updated callbackURL to GOOGLE_CALLBACK_URL

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.CALLBACK_URL
+    callbackURL: process.env.GOOGLE_CALLBACK_URL // ✅ Updated here
 }, (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
 }));
